50352346cd03a3d9f54f66ab7e9ead9c62eeb1d7
StrayCatCannotSwim <liuyulong0101@163.com>
2024-11-03 15:38:17 +0800

    hello页面
    hello world 展示

--- src/main/java/com/lyl/onlineshop/controller/FirstPage.java
+++ src/main/java/com/lyl/onlineshop/controller/FirstPage.java
@@ -1,5 +1,8 @@
 package com.lyl.onlineshop.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class FirstPage {
     @RequestMapping("/hello")
